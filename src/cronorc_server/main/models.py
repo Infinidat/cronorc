02dc193d9f1eb071f92fff288e8c72c9832e7a7f
Itai Shirav <itais@infinidat.com>
2020-01-26 10:51:25 +0200

    Job statuses etc.

--- src/cronorc_server/main/models.py
+++ src/cronorc_server/main/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 
+from djchoices import DjangoChoices, ChoiceItem
+
+
+class JobStatus(DjangoChoices):
+
+    OK       = ChoiceItem()
+    DELAYED  = ChoiceItem()
+    FLAPPING = ChoiceItem()
+    FAILED   = ChoiceItem()
+
 
 class Job(models.Model):
 
     hostname = models.CharField(max_length=255, db_index=True)
     ip = models.GenericIPAddressField()
     command = models.TextField(db_index=True)
+    status = models.CharField(max_length=10, db_index=True, choices=JobStatus.choices, default=JobStatus.OK)
+    last_execution = models.DateTimeField(null=True)
+    last_notification = models.DateTimeField(null=True)
 
     class Meta:
         unique_together = ('hostname', 'ip', 'command')
@@ -14,11 +27,25 @@ class Job(models.Model):
         cmd = self.command[:100] + '...' if len(self.command) > 100 else self.command
         return f'{self.hostname} [{self.ip}] {cmd}'
 
+    def update_status(self):
+        new_status = self.status
+        executions = self.execution_set.order_by('-id')[:5]
+        if executions[0].success():
+            new_status = JobStatus.OK
+        else:
+            new_status = JobStatus.FAILED
+        # TODO identify flapping and delayed jobs
+        if new_status != self.status:
+            self.status = new_status
+            self.save()
+        return self.status
+
 
 class Execution(models.Model):
 
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
     start = models.DateTimeField()
+    notification = models.DateTimeField(null=True)
     elapsed = models.BigIntegerField()
     exit_code = models.IntegerField()
 
--- src/cronorc_server/main/views.py
+++ src/cronorc_server/main/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 
 import json
 
@@ -14,21 +15,35 @@ from .models import Job, Execution
 def notify_view(request):
     message = json.loads(request.body)
     print(message)
-    job, _ = Job.objects.get_or_create(
+    job, _ = Job.objects.update_or_create(
         hostname=message['hostname'],
         ip=message['ip'],
-        command=message['command']
+        command=message['command'],
+        defaults=dict(
+            last_execution=message['start'],
+            last_notification=timezone.now()
+        )
     )
     Execution.objects.create(
         job=job,
         start=message['start'],
         elapsed=message['elapsed'],
-        exit_code=message['exitcode']
+        exit_code=message['exitcode'],
+        notification=timezone.now()
     )
+    job.update_status()
     return HttpResponse('{}', content_type='application/json')
 
 
 @login_required
 def home_view(request):
     executions = Execution.objects.order_by('-id')
+    jobs = Job.objects.order_by('-last_execution')
     return render(request, 'home.html', locals())
+
+
+@login_required
+def job_view(request, id):
+    job = get_object_or_404(Job, id=id)
+    executions = job.execution_set.order_by('-id')
+    return render(request, 'job.html', locals())
